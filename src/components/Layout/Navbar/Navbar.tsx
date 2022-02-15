d873bee21b3261dd24700ea2c8cdcd10b1e3645d
dududornelees <eduardo.dsdorneles@gmail.com>
2022-02-15 14:22:50 -0300

    🔥 New: Navbar

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,8 +1,69 @@
+import { Link, useLocation } from "react-router-dom";
+
 // Styles
 import { NavbarStyle } from "./styles";
 
+// Icons
+import { AiFillHome } from "react-icons/ai";
+import { FaUsers, FaUserAlt } from "react-icons/fa";
+import { AiFillSchedule } from "react-icons/ai";
+
 const Navbar: React.FC = () => {
-  return <NavbarStyle>Navbar!</NavbarStyle>;
+  const { pathname } = useLocation();
+
+  return (
+    <NavbarStyle>
+      <div className="navbar__logo">
+        <img src="/logo.png" alt="Logo da Empresa" />
+        <h2>Nome e Logo</h2>
+      </div>
+
+      <div className="navbar__menu">
+        <ul>
+          <li>
+            <Link className={pathname == "/home" ? "active" : ""} to="/home">
+              <span className="active-dash"></span>
+              <AiFillHome />
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              className={pathname == "/clients" ? "active" : ""}
+              to="/clients"
+            >
+              <span className="active-dash"></span>
+              <FaUsers />
+              Clientes
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              className={pathname == "/schedules" ? "active" : ""}
+              to="/schedules"
+            >
+              <span className="active-dash"></span>
+              <AiFillSchedule />
+              Agendamentos
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              className={pathname == "/my-profile" ? "active" : ""}
+              to="/my-profile"
+            >
+              <span className="active-dash"></span>
+              <FaUserAlt />
+              Meu perfil
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </NavbarStyle>
+  );
 };
 
 export default Navbar;
--- src/components/Layout/Navbar/styles.ts
+++ src/components/Layout/Navbar/styles.ts
@@ -5,4 +5,67 @@ export const NavbarStyle = styled.div`
   width: 255px;
   min-height: 100vh;
   height: 100%;
+
+  .navbar__logo {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      width: 32px;
+      margin-right: 12px;
+    }
+
+    h2 {
+      font-size: 19px;
+      font-weight: bold;
+      color: #a4a6b3;
+    }
+  }
+
+  .navbar__menu ul {
+    li {
+      list-style: none;
+      height: 100%;
+      width: 100%;
+
+      a {
+        transition: ${(props) => props.theme.transition};
+        padding: 20px 32px;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: #a4a6b3;
+        position: relative;
+
+        .active-dash {
+          height: 100%;
+          width: 3px;
+          display: none;
+          position: absolute;
+          left: 0;
+          background-color: #dde2ff;
+        }
+
+        svg {
+          margin-right: 22px;
+          font-size: 16px;
+        }
+
+        &:hover {
+          background-color: #3f4049;
+        }
+
+        &.active {
+          background-color: #3f4049;
+          color: #dde2ff;
+
+          .active-dash {
+            display: block;
+          }
+        }
+      }
+    }
+  }
 `;
